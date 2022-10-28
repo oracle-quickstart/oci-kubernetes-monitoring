2859615d84d90325f9db8c3ffa0e60d8ef546a5b
paliwalparitosh <paritoshedx@gmail.com>
2022-10-28 10:39:17 +0530

    use terraform data resource to pull cluster name instead of local-exec provisioner

--- logan/terraform/oke/oke.tf
+++ logan/terraform/oke/oke.tf
@@ -1,36 +1,21 @@
 locals {
-  cluster_details_map = jsondecode(data.local_file.cluster_details.content)
-  cluster_name        = tostring(local.cluster_details_map["data"]["name"])
+  oke_cluster_ocid = var.oke_cluster_ocid
+  oke_compartment_ocid = var.oke_compartment_ocid
+
+  oke_clusters_list = data.oci_containerengine_clusters.oke_clusters_list.clusters
+  cluster_name = [ for c in local.oke_clusters_list : c.name if c.id == local.oke_cluster_ocid ][0]
 }
 
 data "oci_containerengine_cluster_kube_config" "oke" {
-  cluster_id = var.oke_cluster_ocid
+  cluster_id = local.oke_cluster_ocid
 }
 
-data "local_file" "cluster_details" {
-  filename   = "${path.module}/local/clusterDetails.json"
-  depends_on = [null_resource.cluster_details]
+data "oci_containerengine_clusters" "oke_clusters_list" {
+    compartment_id = local.oke_compartment_ocid
 }
 
 # kubeconfig when using Terraform locally. Not used by Oracle Resource Manager
 resource "local_file" "oke_kubeconfig" {
   content  = data.oci_containerengine_cluster_kube_config.oke.content
   filename = "${path.module}/local/kubeconfig"
-}
-
-# This resource generates /local/clusterDetails.json which is used to parse OKE Cluster Name
-resource "null_resource" "cluster_details" {
-
-  triggers = {
-    #cluster_id = var.oke_cluster_ocid
-    whenOkeConfigUpdates = data.oci_containerengine_cluster_kube_config.oke.content
-  }
-
-  provisioner "local-exec" {
-    command = "oci ce cluster get --cluster-id=$CLUSTER_ID > $OUTPUT_FILE"
-    environment = {
-      CLUSTER_ID  = "${var.oke_cluster_ocid}"
-      OUTPUT_FILE = "${path.module}/local/clusterDetails.json"
-    }
-  }
 }
