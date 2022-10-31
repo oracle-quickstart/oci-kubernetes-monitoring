fd30837bcf30fe6fe362bf5e143e555906aa71dd
paliwalparitosh <paritoshedx@gmail.com>
2022-10-31 14:33:03 +0530

    refer to resources instead of locals

--- logan/terraform/oke/outputs.tf
+++ logan/terraform/oke/outputs.tf
@@ -3,9 +3,9 @@ output "cluster_name" {
 }
 
 output "policy_name" {
-  value = local.policy_name
+  value = oci_identity_policy.oke_dynamic_group_policies[0].name
 }
 
 output "dynamic_group_name" {
-  value = local.dynamic_group_name
+  value = oci_identity_dynamic_group.oke_dynamic_group[0].name
 }
