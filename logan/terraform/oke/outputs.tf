732823a0dbc123cb89eed16218d77e25b477d850
paliwalparitosh <paritoshedx@gmail.com>
2022-10-31 20:27:10 +0530

    add check for existing policies and dynamic group for oke monitoring

--- logan/terraform/oke/outputs.tf
+++ logan/terraform/oke/outputs.tf
@@ -1,3 +1,11 @@
 output "cluster_name" {
   value = local.cluster_name
 }
+
+output "dynamic_group_already_exists" {
+  value = local.dynamic_group_already_exists
+}
+
+output "oke_monitoring_policy_already_exists" {
+  value = local.oke_monitoring_policy_already_exists
+}
