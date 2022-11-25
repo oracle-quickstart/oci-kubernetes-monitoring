c85a962d206e7328ad6c780200b4b8938b3f00c0
papaliwa <paritosh.paliwal@oracle.com>
2022-11-25 18:59:46 +0530

    remove check for existing policies and dynamic groups

--- logan/terraform/oke/outputs.tf
+++ logan/terraform/oke/outputs.tf
@@ -1,11 +1,3 @@
 output "cluster_name" {
   value = local.cluster_name
 }
-
-output "dynamic_group_already_exists" {
-  value = local.dynamic_group_already_exists
-}
-
-output "oke_monitoring_policy_already_exists" {
-  value = local.oke_monitoring_policy_already_exists
-}
