dcb58e23972c6419990bd768d571633a91002377
Santhosh Kumar Vuda <santhoshkumarvuda@MacBook-Pro.local>
2023-02-07 22:52:46 +0530

    Release v2.0.1, 2022-02-07

---
+++ logan/terraform/oke/modules/dashboards/dashboard.tf
@@ -0,0 +1,8 @@
+locals {
+  dashboards = ["cluster.json", "node.json", "pod.json", "workload.json"]
+}
+
+resource "oci_management_dashboard_management_dashboards_import" "multi_management_dashboards_import" {
+  for_each       = toset(local.dashboards)
+  import_details = templatefile(format("%s/%s/%s", "${path.module}", "dashboards_json", each.value), { "compartment_ocid" : "${var.compartment_ocid}" })
+}
