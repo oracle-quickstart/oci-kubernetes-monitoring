cde450eb581e29f73a761bf4abc200957cae1cb5
kumar-ola <kumar.varun@oracle.com>
2023-05-08 19:16:41 -0700

    documentation and license updates
    Reviewed and added README for values, added license text

--- logan/docker-images/v1.0/debian/entrypoint.sh
+++ logan/docker-images/v1.0/debian/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
-# # Copyright (c) 2023, Oracle and/or its affiliates.
-# # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
-#
-# Copyright (c) 2023, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
-
 # Copyright (c) 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
