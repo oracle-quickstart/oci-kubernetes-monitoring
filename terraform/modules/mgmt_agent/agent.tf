3475bf6236ee78627bd2ca5d103d29fbb738866e
paliwalparitosh <paritoshedx@gmail.com>
2023-06-12 21:32:45 +0530

    refactor

--- terraform/modules/mgmt_agent/agent.tf
+++ terraform/modules/mgmt_agent/agent.tf
@@ -3,10 +3,10 @@
 
 locals {
   installKey          = oci_management_agent_management_agent_install_key.Kubernetes_AgentInstallKey.key
-  inputRspFileContent = base64encode(join("\n", ["ManagementAgentInstallKey = ${local.installKey}", "AgentDisplayName = k8_macs_agent-${var.uniquifier}"]))
+  inputRspFileContent = base64encode(join("\n", ["ManagementAgentInstallKey = ${local.installKey}", "AgentDisplayName = k8_mgmt_agent_agent-${var.uniquifier}"]))
 }
 
 resource "oci_management_agent_management_agent_install_key" "Kubernetes_AgentInstallKey" {
   compartment_id = var.compartment_ocid
-  display_name   = "k8_macs_key-${var.uniquifier}"
+  display_name   = "k8_mgmt_agent_key-${var.uniquifier}"
 }
