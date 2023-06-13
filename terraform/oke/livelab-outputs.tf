4e1bda0582a55b9c00746577eb37f4dd8006efe5
paliwalparitosh <paritoshedx@gmail.com>
2023-06-13 22:16:16 +0530

    add view dashboard button for livelab

---
+++ terraform/oke/livelab-outputs.tf
@@ -0,0 +1,6 @@
+# Copyright (c) 2023, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+output livelab_button_link  {
+    value = "https://www.oracle.com/cloud/sign-in.html?redirect_uri=https%3A%2F%2Fcloud.oracle.com%2Floganalytics%2Fdashboards%3Fid%3DdashboardHome%26comp%3Docid1.compartment.oc1..aaaaaaaa66eembgbbw4y5aqqt5obj2bu7wsdlamckmmdmzg6nr6cg7my7lkq%26region%3Dus-phoenix-1"
+}
