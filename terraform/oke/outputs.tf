b5ecef47dd7cb916e8c4f833b1771354a68f9136
paliwalparitosh <paritoshedx@gmail.com>
2023-08-24 16:21:12 +0530

    updates output to generate helm install command

--- terraform/oke/outputs.tf
+++ terraform/oke/outputs.tf
@@ -2,7 +2,7 @@
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 locals {
-  output_external_values_ymal = yamlencode({
+  output_helm_external_values = yamlencode({
     "global" = {
       "kubernetesClusterID"   = var.oke_cluster_ocid
       "kubernetesClusterName" = local.oke_cluster_name
@@ -17,12 +17,25 @@ locals {
       }
     }
   })
+
+  output_helm_command_2 = join("\n" , [
+    "helm install oci-kubernetes-monitoring oci-onm/oci-onm \\",
+    "--set global.kubernetesClusterID=${var.oke_cluster_ocid} \\",
+    "--set global.kubernetesClusterName=${local.oke_cluster_name} \\",
+    "--set oci-onm-logan.ociLALogGroupID=${module.loggingAnalytics[0].oci_la_logGroup_ocid} \\",
+    "--set oci-onm-logan.ociLANamespace=${module.loggingAnalytics[0].oci_la_namespace} \\",
+    "--set oci-onm-mgmt-agent.mgmtagent.installKeyFileContent=${module.management_agent[0].mgmt_agent_install_key_content}"
+  ])
 }
 
-output "helm_command_1" {
+output helm_command_1 {
   value = "helm repo add oci-onm https://oracle-quickstart.github.io/oci-kubernetes-monitoring"
 }
 
-output "helm_command_2" {
-  value = local.deploy_helm_ui_option ? null : local.output_external_values_ymal
+output helm_command_2 {
+  value = local.output_helm_command_2
 }
+
+/* output "helm_external_values" {
+  value = local.deploy_helm_ui_option ? null : local.output_helm_external_values
+} */
