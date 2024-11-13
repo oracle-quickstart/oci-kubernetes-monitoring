b2dfc0f0993265ca346ed49adc2219eea4cbda5b
paliwalparitosh <paritoshedx@gmail.com>
2024-11-13 18:38:00 +0530

    adds copyright banner

--- terraform/oke/resources/metadata.sh
+++ terraform/oke/resources/metadata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright (c) 2024, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 set -e
 curl -H "Authorization: Bearer Oracle" -sL http://169.254.169.254/opc/v2/instance/ | jq .regionInfo
 # example output =>
--- terraform/oke/resources/oke-status-check.sh
+++ terraform/oke/resources/oke-status-check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright (c) 2024, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 ############################################################
 ##
