a929e2bf6cf3b59892462cebb34243336bdae7e6
Santhosh Kumar Vuda <santhosh_auce@yahoo.com>
2025-02-03 10:28:13 +0530

    v3.5.3

--- logan/docker-images/v1.0/debian-deprecated/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-# Copyright (c) 2023, 2024, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-exec fluentd -c ${FLUENTD_CONF} -p /fluentd/plugins --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
